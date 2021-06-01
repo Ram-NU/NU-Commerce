b95bd456540bcbf737796c7025373d579c62fef7
unknown <vigneshwarram.balakrishnan@nutechnologyinc.com>
2021-06-02 00:05:54 +0530

    EOD Updation

--- src/header/Bodyhome.js
+++ src/header/Bodyhome.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Body_home.css';
-import Card_view from './Card_view.js';   
+import CardView from './Card-view.js'; 
+
+const al=()=>{
+    alert("Hai!");
+}    
 
 class Bodyhome extends React.Component{
     render(){
         return(
             <div className="body">
+                <div className="bg-img">
+                    <img src="" alt=""/>
+                 </div>   
+                <div className="message">
+                    <p>
+                        Welcome to NU Commerce! Enjoy purchasing this week full of offer upto 50% Click here:
+                        <a onClick={al}>NU-Commerce.com</a>
+                    </p>
+                </div>    
                 <div className="container">
-                   <Card_view/>
+                   <CardView/>
                 </div>
             </div>
         );
---
+++ src/header/Card-view.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Card-view.css';
+import image1 from 'C:/react/NU-Commerce/src/header/main-background.jpg';
+
+class CardView extends React.Component {
+    render(){
+        return(
+            <div className="card-container">
+                <div className="card-title">
+                    Electronics
+                 </div>   
+                <div className="card-image">
+                    <img src={image1} alt="card-img"/>          
+                </div>
+                <div className="card-button">
+                    <button className>View</button>
+                </div>        
+            </div>    
+        );
+    }      
+} 
+
+export default CardView
