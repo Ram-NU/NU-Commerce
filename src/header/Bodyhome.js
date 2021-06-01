1e813c38496e6462a9de53f29a54b36aadf82893
unknown <vigneshwarram.balakrishnan@nutechnologyinc.com>
2021-06-02 00:36:06 +0530

    EOD Updated

--- src/header/Bodyhome.js
+++ src/header/Bodyhome.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Body_home.css';
 import CardView from './Card-view.js'; 
+   
 
-const al=()=>{
-    alert("Hai!");
-}    
+const url="https://nutechnologyinc.com";
 
 class Bodyhome extends React.Component{
     render(){
@@ -16,7 +15,7 @@ class Bodyhome extends React.Component{
                 <div className="message">
                     <p>
                         Welcome to NU Commerce! Enjoy purchasing this week full of offer upto 50% Click here:
-                        <a onClick={al}>NU-Commerce.com</a>
+                        <a href={url}> NU-Commerce Official Website</a>
                     </p>
                 </div>    
                 <div className="container">
