667f568c9cfbb0f03516365d2d335cf5dd04049e
unknown <vigneshwarram.balakrishnan@nutechnologyinc.com>
2021-06-01 22:11:30 +0530

    updation

--- src/header/Bodyhome.js
+++ src/header/Bodyhome.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './Body_home.css';   
+import './Body_home.css';
+import Card_view from './Card_view.js';   
 
 class Bodyhome extends React.Component{
     render(){
         return(
             <div className="body">
                 <div className="container">
-                   
+                   <Card_view/>
                 </div>
             </div>
         );
