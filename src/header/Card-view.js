a2b4ce736f91c8a964708b1d4408b8935546141e
unknown <vigneshwarram.balakrishnan@nutechnologyinc.com>
2021-06-02 09:22:48 +0530

    Card View Updation

--- src/header/Card-view.js
+++ src/header/Card-view.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Card-view.css';
 import image1 from 'C:/react/NU-Commerce/src/header/main-background.jpg';
 
+const al=()=>{
+    alert("Hai!");
+}    
+
 class CardView extends React.Component {
     render(){
         return(
@@ -13,7 +17,7 @@ class CardView extends React.Component {
                     <img src={image1} alt="card-img"/>          
                 </div>
                 <div className="card-button">
-                    <button className>View</button>
+                    <button className onClick={al}>View</button>
                 </div>        
             </div>    
         );
