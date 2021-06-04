be07f07984646bd4bc06a42a8b3791a00fe3cb00
unknown <vigneshwarram.balakrishnan@nutechnologyinc.com>
2021-06-04 12:22:54 +0530

    Responsive Edit

--- src/header/Card-view.js
+++ src/header/Card-view.js
@@ -11,7 +11,7 @@ class CardView extends React.Component {
         return(
             <div>
             {Categories.map((items)=>(
-                <section className="inline flow-root list-item card-container">                    
+                <section className="card-container">                    
                     
                     <section className="card-title">
                         {items.category}
